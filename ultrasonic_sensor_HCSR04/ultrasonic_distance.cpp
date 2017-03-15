7b8887f36a104569510263c3d13c508b97509cbd
Naurislv <naurisdorbe@gmail.com>
2017-03-15 21:15:45 +0200

    Add initial code to receive distance from ultrasonic sensors.

---
+++ ultrasonic_sensor_HCSR04/ultrasonic_distance.cpp
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <wiringPi.h>
+
+#define TRUE 1
+
+#define TRIG 5
+#define ECHO 6
+
+void setup() {
+    wiringPiSetup();
+    pinMode(TRIG, OUTPUT);
+    pinMode(ECHO, INPUT);
+
+    //TRIG pin must start LOW
+    digitalWrite(TRIG, LOW);
+    delay(30);
+}
+
+int getCM() {
+    //Send trig pulse
+    digitalWrite(TRIG, HIGH);
+    delayMicroseconds(20);
+    digitalWrite(TRIG, LOW);
+
+    //Wait for echo start
+    while(digitalRead(ECHO) == LOW);
+
+    //Wait for echo end
+    long startTime = micros();
+    while(digitalRead(ECHO) == HIGH);
+    long travelTime = micros() - startTime;
+
+    //Get distance in cm
+    int distance = travelTime / 58;
+
+    return distance;
+}
+
+int main(void) {
+    setup();
+
+    printf("Distance: %dcm\n", getCM());
+
+    return 0;
+}
