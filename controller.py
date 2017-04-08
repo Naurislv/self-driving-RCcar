661a66729840b3bd3c4d451a11bcead7c9d5e18a
Naurislv <naurisdorbe@gmail.com>
2017-04-08 22:45:47 +0300

    Rewrite controller.py for new converter.

--- controller.py
+++ controller.py
@@ -1,31 +1,54 @@
 """Controls for Wltoys A969 model car motor/steering actuators (from Raspberry Pi).
 
-Usage:
+Simple test program:
+    import time
     import wltoys_a969
-    Set speed
-    wltoys_a969.speed_goal_set(Speed)    #  Speed in [0..12] (slower..faster)
 
-    Set steering angle
-    wltoys_a969.steer_goal_set(Angle)    # Angle in [-18..18] (in degrees)
+    time.sleep(1)
+
+    wltoys_a969.steer_goal_set(10)  # Angle in [-18..18] (in degrees)
+    wltoys_a969.speed_goal_set(0)  # Speed in [0..x] (slower..faster)
+    time.sleep(1.3)
+
+    wltoys_a969.steer_goal_set(10)
+    wltoys_a969.speed_goal_set(20)
+    time.sleep(1.3)
+
+    wltoys_a969.steer_goal_set(0)
+    wltoys_a969.speed_goal_set(0)
+    time.sleep(5.0)
+
+
+    wltoys_a969.stop()
 """
 
 import RPi.GPIO as IO
 import Adafruit_ADS1x15
 import time
+import threading
+
 
 IO.setwarnings(False)
-IO.setmode(IO.BCM)
+IO.setmode (IO.BCM)
 
-IO.setup(19, IO.OUT)
-pin_motor_fwd = IO.PWM(19, 50)
-pin_motor_fwd.start(100)
+IO.setup(19,IO.OUT)
+IO.output(19,IO.LOW)
+pin_motor_fwd = IO.PWM(19,2250)
+pin_motor_fwd.start(0)
 
-IO.setup(26, IO.OUT)
-pin_steer_right = IO.PWM(26, 100)
+IO.setup(16,IO.OUT)
+IO.output(16,IO.LOW)
+pin_motor_bwd = IO.PWM(16,2250)
+pin_motor_bwd.start(0)
+
+IO.setup(26,IO.OUT)
+IO.output(26,IO.LOW)
+pin_steer_right = IO.PWM(26,100)
 pin_steer_right.start(0)
 
-IO.setup(20, IO.OUT)
-pin_steer_left = IO.PWM(20, 100)
+IO.setup(20,IO.OUT)
+IO.output(20,IO.LOW)
+pin_steer_left = IO.PWM(20,100)
 pin_steer_left.start(0)
 
 adc = Adafruit_ADS1x15.ADS1015()
@@ -33,52 +56,53 @@ ADC_GAIN = 1
 
 
 # parameters
-MOTOR_PWM_MAX = 100
-MOTOR_PWM_1 = 0
-MOTOR_PWM_2 = 12
+MOTOR_PWM_MAX          =      30
 
-STEER_MIN = 1123  # Steer servo min feedback value
-STEER_MAX = 837  # Steer servo max feedback value
-STEER_PWM_T = 25 / 2.55  # Steer servo threshold PWM abs value
-STEER_PWM_MAX = (255 - STEER_PWM_T) / 2.55  # Steer servo max PWM abs value
-STEER_MIN_ANGLE = -18.0  # Steer servo min angle value
-STEER_IDLE_ANGLE = 0.0  # Steer servo idle angle value
-STEER_MAX_ANGLE = 18.0  # Steer servo max angle value
+STEER_MIN              =    1123   		# Steer servo min feedback value
+STEER_MAX              =     837   		# Steer servo max feedback value
+STEER_PWM_T            = 25/2.55   	# Steer servo threshold PWM abs value
+STEER_PWM_MAX          =     (255-STEER_PWM_T)/2.55   	# Steer servo max PWM abs value
+STEER_MIN_ANGLE        =   -18.0   		# Steer servo min angle value
+STEER_IDLE_ANGLE       =     0.0   		# Steer servo idle angle value
+STEER_MAX_ANGLE        =    18.0   		# Steer servo max angle value
+
+# command functions
+running = True
+def stop():
+	global running
+	running = False
+	return
 
 # goal values
 goal_speed = 0
 goal_steer_angle = 0
 
-
 # goal assign functions
 def speed_goal_set(speed):
-    """Speed goal set."""
-    global goal_speed
-    goal_speed = speed
-    return
-
+	global goal_speed
+	goal_speed = speed
+	return
 
 def steer_goal_set(angle):
-    """Steer goal set."""
-    global goal_steer_angle
-    if (angle > STEER_MAX_ANGLE):
-        angle = STEER_MAX_ANGLE
-    elif (angle < STEER_MIN_ANGLE):
-        angle = STEER_MIN_ANGLE
-    goal_steer_angle = angle
-    return
-
+	global goal_steer_angle
+	if (angle>STEER_MAX_ANGLE):
+		angle = STEER_MAX_ANGLE
+	elif (angle<STEER_MIN_ANGLE):
+		angle = STEER_MIN_ANGLE
+	goal_steer_angle = angle
+	return
 
 # low level main motor control
 def motor_set(motor_pwm_val):
-    """Motor set."""
-    if motor_pwm_val > MOTOR_PWM_2:
-        motor_pwm_val = MOTOR_PWM_2
-    elif motor_pwm_val < MOTOR_PWM_1:
-        motor_pwm_val = MOTOR_PWM_1
-    pwm = MOTOR_PWM_MAX - motor_pwm_val
-    pin_motor_fwd.ChangeDutyCycle(pwm)
-    return
+	if (motor_pwm_val < -MOTOR_PWM_MAX):
+		motor_pwm_val = -MOTOR_PWM_MAX
+	elif (motor_pwm_val > MOTOR_PWM_MAX):
+		motor_pwm_val = MOTOR_PWM_MAX
+
+	pwm = abs(motor_pwm_val)
+
+	pin_motor_fwd.ChangeDutyCycle(pwm)
+        return
 
 
 # low level steering servo control
@@ -122,7 +146,7 @@ def read_steer_angle():
 
 # high-level main motor control
 def motor_step():
-	if goal_speed > 0.01:
+	if abs(goal_speed) > 0.01:
 		motor_set(goal_speed)
 	else:
 		motor_set(0)
@@ -140,11 +164,10 @@ steer_e = 100
 theta_diff = 0
 v_steer = 0
 sig_steer = 0
-t_prev = 0
 
+t_prev = 0
 # high-level steering servo control (PID control)
 def steer_step():
-	"""Steer step."""
 	global steer_kp
 	global steer_ki
 	global steer_kd
@@ -179,8 +202,24 @@ def steer_step():
 
 
 def act_loop():
-	while (True):
+	global running
+	while (running):
 		steer_step()
 		motor_step()
 		time.sleep(0.0001)
 	return
+
+
+class myThread (threading.Thread):
+    def __init__(self, threadID, name):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+    def run(self):
+        print "Starting: " + self.name
+        act_loop()
+        print "Stopping: " + self.name
+
+
+thread1 = myThread(1, "Actuator loop")
+thread1.start()
