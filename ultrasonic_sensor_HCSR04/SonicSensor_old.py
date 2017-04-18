2a2970c682c025e364fc683b25800c81acfefbe6
Naurislv <naurisdorbe@gmail.com>
2017-04-18 13:00:09 +0000

    Get smallest latency possible with for SonicSensor_old.py

--- ultrasonic_sensor_HCSR04/SonicSensor_old.py
+++ ultrasonic_sensor_HCSR04/SonicSensor_old.py
@@ -15,29 +15,32 @@ GPIO.output(TRIG, False)  # Set TRIG as LOW
 print("Waitng For Sensor To Settle")
 time.sleep(2)  # Delay of 2 seconds
 
+oor = False
 while True:
+    start_time = time.time()
 
     GPIO.output(TRIG, True)  # Set TRIG as HIGH
-    time.sleep(0.00001)  # Delay of 0.00001 seconds
+    time.sleep(0.0001)  # Delay of 0.00001 seconds
     GPIO.output(TRIG, False)  # Set TRIG as LOW
 
-    start_test = time.time()
     while GPIO.input(ECHO) == 0:  # Check whether the ECHO is LOW
         pulse_start = time.time()  # Saves the last known time of LOW pulse
-    print(time.time() - start_test)
 
-    start_test = time.time()
-    while GPIO.input(ECHO) == 1:  # Check whether the ECHO is HIGH
-        pulse_end = time.time()  # Saves the last known time of HIGH pulse
-        print(pulse_start)
-    print(time.time() - start_test)
-
-    pulse_duration = pulse_end - pulse_start  # Get pulse duration to a variable
-
+    pulse_duration = 0
+    while GPIO.input(ECHO) == 1 and pulse_duration < 0.005:  # Check whether the ECHO is HIGH
+        pulse_duration = time.time() - pulse_start  # Get pulse duration to a variable
+    
     distance = pulse_duration * 17150  # Multiply pulse duration by 17150 to get distance
-    distance = round(distance, 2)  # Round to two decimal points
-
-    if distance > 2 and distance < 400:  # Check whether the distance is within range
-        print("Distance:", distance - 0.5, "cm")  # Print distance with 0.5 cm calibration
+    distance = round(distance, 3)  # Round to two decimal points
+    
+    print(round(time.time() - start_time, 5), end = '  --> ')
+    if distance > 2 and distance < 40:  # Check whether the distance is within range
+        if oor:
+            print('Out Of Range')
+            oor = False
+        else:
+            print("Distance:", distance, "cm")  # Print distance with 0.5 cm calibration
     else:
         print("Out Of Range")  # display out of range
+        oor = True
+    time.sleep(0.01)
