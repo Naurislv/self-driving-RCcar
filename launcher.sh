6e669c190f5b74b7305f3c752e5a15f90ffaee0d
Naurislv <naurisdorbe@gmail.com>
2017-04-25 15:46:43 +0300

    Change image height to 110 from 100 in launcher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # launcher.sh
 
-sudo /home/pi/miniconda3/bin/python /home/pi/self-driving-RCcar/drive_me.py 14 280 100
+sudo /home/pi/miniconda3/bin/python /home/pi/self-driving-RCcar/drive_me.py 14 280 110
