c2fba4429307c7974ae3ba4694a6510d1b0a70dc
naurislv <naurisdorbe@gmail.com>
2017-07-31 20:11:24 +0300

    Modify model.py and dirving_log.py for pyling standards and while training now convert angle to radius.

--- keras_models/driving_log.py
+++ keras_models/driving_log.py
@@ -1,42 +1,63 @@
+"""Create driving log from image filenames."""
+
 import sys
 import glob
 import os
 import logging
 import re
+import math
 
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
 
 
 def _re(path):
+    """Regex to find necesarry information from filename."""
+
     return re.findall(r"^([0-9]{6})_([CLR])_\(([-.0-9]+), ([-.0-9]+)", path)[0]
 
+def angle2tcr(angle, wheelbase):
+    """Convert cars steering angle to Turning Circle Radius.
 
-def read(path, steering_normalizer):
+    Inputs:
+        wheelbase: distance between wheels centers in mm
+        angle: steering angle in degrees
+
+    Resources:
+        https://www.quora.com/What-is-the-method-to-calculate-turning-radius-if-
+        only-the-steering-ratio-and-vehicles-basic-dimensions-are-known
+    """
+
+    return wheelbase / math.sin(math.radians(angle))
+
+def read(path, wheelbase):
     """Read driving_log from image names"""
 
     sub_dirs = [x[0] + '/' for x in os.walk(path)][1:]
 
-    logging.info('Found {} directories'.format(len(sub_dirs)))
+    logging.info('Found %d directories', len(sub_dirs))
 
     ret_dict = {}
     for sdir in sub_dirs:
         im_paths = glob.glob(sdir + '*.jpeg')
         im_paths_slices = [os.path.splitext(os.path.basename(im))[0] for im in im_paths]
 
-        for i in range(len(im_paths)):
-            ID, pos, steer, thorttle = _re(im_paths_slices[i])
+        for i, im_p in enumerate(im_paths):
+            id_nr, pos, steer, thorttle = _re(im_paths_slices[i])
 
-            if ID not in ret_dict:
-                ret_dict[ID] = {}
+            if id_nr not in ret_dict:
+                ret_dict[id_nr] = {}
 
-            steer = float(steer) / steering_normalizer
-            ret_dict[ID][pos] = (im_paths[i], steer, thorttle)
+            tcr = angle2tcr(steer, wheelbase)
 
-    logging.info('Found {} image triplets (L, C, R)'.format(len(ret_dict)))
+            ret_dict[id_nr][pos] = (im_p, tcr, thorttle)
+
+    logging.info('Found %d image triplets (L, C, R)', len(ret_dict))
 
     return ret_dict
 
 
 if __name__ == "__main__":
-    path = sys.argv[1]
-    path_dict = read(path)
+    PATH = sys.argv[1]
+    PATH_DICT = read(PATH, 20)
--- keras_models/model.py
+++ keras_models/model.py
@@ -3,50 +3,50 @@
 More info in README.md
 
 """
+# Standard imports
+import logging
+import math
+
+# Local imports
 import driving_log
 
+# ML imports
 from keras.models import Sequential
 from keras.layers import Dense, Flatten, Conv2D, Dropout
 from keras.layers.normalization import BatchNormalization
 from keras.optimizers import adam
-
 import tensorflow as tf
 
+# Other imports
 import cv2
 import numpy as np
 from scipy.misc import imresize
 
-import logging
-logging.basicConfig(format='%(asctime)s %(message)s', datefmt='%m/%d/%Y %H:%M:%S', level=logging.INFO)
+logging.basicConfig(format='%(asctime)s %(message)s',
+                    datefmt='%m/%d/%Y %H:%M:%S',
+                    level=logging.INFO)
 
 
-class train_path_error(Exception):
-    """Custom Training log error,- happens when argument is not passed."""
-
-    pass
-
-
-flags = tf.app.flags
-FLAGS = flags.FLAGS
+_FLAGS = tf.app.flags
+FLAGS = _FLAGS.FLAGS
 
 # Input arguments
-
-flags.DEFINE_string('train_path', '', "Training images directory.")
-flags.DEFINE_string('val_path', '', "Validation images directory.")
-flags.DEFINE_string('output_model', '', "Directory to save keras model. e.g. /home/user/udacity/")
-flags.DEFINE_string('keras_weights', '', "Pretrained wieghts to use for training. Keras h5 file.")
-flags.DEFINE_integer('epochs', 10, "Training epoch count.")
-flags.DEFINE_integer('batch_size', 256, "Batch size")
-flags.DEFINE_integer('dropout', 0, "0 - Disable | 1 - Enable dropout.")
+_FLAGS.DEFINE_string('train_path', '', "Training images directory.")
+_FLAGS.DEFINE_string('val_path', '', "Validation images directory.")
+_FLAGS.DEFINE_string('output_model', '', "Directory to save keras model. e.g. /home/user/udacity/")
+_FLAGS.DEFINE_string('keras_weights', '', "Pretrained wieghts to use for training. Keras h5 file.")
+_FLAGS.DEFINE_integer('epochs', 10, "Training epoch count.")
+_FLAGS.DEFINE_integer('batch_size', 256, "Batch size")
+_FLAGS.DEFINE_integer('dropout', 0, "0 - Disable | 1 - Enable dropout.")
 
 if FLAGS.train_path == '':
-    raise train_path_error('Please provide argument : --train_path path_to_training_images')
+    raise OSError('Please provide argument : --train_path path_to_training_images')
 
 
 class Utils(object):
     """Helping functions."""
 
-    def load_data(self, drive_info, Path, use_left_right=True):
+    def load_data(self, drive_info, use_left_right=True):
         """Load images from files.
 
         Randomly will load one of three possible images for each path - left, center or right.
@@ -63,13 +63,13 @@ class Utils(object):
 
             if use_left_right:
                 i_lrc = np.random.randint(3)
-                if (i_lrc == 0):
+                if i_lrc == 0:
                     im_path, steer, thorttle = drive_info[key]['L']
                     shift_ang = .18
-                if (i_lrc == 1):
+                if i_lrc == 1:
                     im_path, steer, thorttle = drive_info[key]['C']
                     shift_ang = 0.
-                if (i_lrc == 2):
+                if i_lrc == 2:
                     im_path, steer, thorttle = drive_info[key]['R']
                     shift_ang = -.18
             else:
@@ -90,25 +90,25 @@ class Utils(object):
         new_h = 66
         res_images = np.zeros((images.shape[0], new_h, new_w, images.shape[3]), dtype=images.dtype)
 
-        for i in range(len(images)):
-            res_images[i] = imresize(images[i], (new_h, new_w))
+        for i, img in enumerate(images):
+            res_images[i] = imresize(img, (new_h, new_w))
 
         return res_images
 
-    def save_keras_model(self, model, path):
+    def save_keras_model(self, save_model, path):
         """Save keras model to given path."""
-        model.save_weights(path + 'model.h5')
+        save_model.save_weights(path + 'model.h5')
 
         with open(path + 'model.json', "w") as text_file:
-            text_file.write(m.to_json())
+            text_file.write(save_model.to_json())
 
         logging.info('\n\nKeras model saved.')
 
     def random_brightness(self, images):
         """Add random brightness to give image dataset to imitate day/night."""
 
-        for i in range(len(images)):
-            image1 = cv2.cvtColor(images[i], cv2.COLOR_RGB2HSV)
+        for i, img in enumerate(images):
+            image1 = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
             random_bright = .25 + np.random.uniform()
             image1[:, :, 2] = image1[:, :, 2] * random_bright
             image1 = cv2.cvtColor(image1, cv2.COLOR_HSV2RGB)
@@ -118,41 +118,43 @@ class Utils(object):
         return images
 
     def random_shifts(self, images, labels, h_shift, v_shift):
-        """Add random horizontal/vertical shifts to image dataset to imitate steering away from sides."""
+        """Add random horizontal/vertical shifts to image dataset to imitate
+        steering away from sides."""
 
         rows = images[0].shape[0]
         cols = images[0].shape[1]
 
-        for i in range(len(images)):
+        for i, img in enumerate(images):
             horizontal = h_shift * np.random.uniform() - h_shift / 2
             vertical = v_shift * np.random.uniform() - v_shift / 2
 
-            M = np.float32([[1, 0, horizontal], [0, 1, vertical]])
+            warp_m = np.float32([[1, 0, horizontal], [0, 1, vertical]])
 
             # change also corresponding lable -> steering angle
             labels[i] = labels[i] + horizontal / h_shift * 2 * .2
-            images[i] = cv2.warpAffine(images[i], M, (cols, rows))
+            images[i] = cv2.warpAffine(img, warp_m, (cols, rows))
 
         return images, labels
 
-    def cut_rows(self, dataset, up=0, down=0):
-        """Remove specific rows from up and down from given image dataset."""
-        return dataset[:, 0 + up:160 - down, :, :]
+    def cut_rows(self, dataset, top=0, buttom=0):
+        """Remove specific rows from top and buttom from given image dataset."""
+        return dataset[:, 0 + top:160 - buttom, :, :]
 
     def random_shadows(self, images):
         """Add random shadows to given image dataset. It helps model to generalize better."""
 
-        for i in range(len(images)):
+        for i, img in enumerate(images):
             top_y = 320 * np.random.uniform()
             top_x = 0
             bot_x = 160
             bot_y = 320 * np.random.uniform()
-            image_hls = cv2.cvtColor(images[i], cv2.COLOR_RGB2HLS)
+            image_hls = cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
             shadow_mask = 0 * image_hls[:, :, 1]
-            X_m = np.mgrid[0:images[i].shape[0], 0:images[i].shape[1]][0]
-            Y_m = np.mgrid[0:images[i].shape[0], 0:images[i].shape[1]][1]
+            x_m = np.mgrid[0:img.shape[0], 0:img.shape[1]][0]
+            y_m = np.mgrid[0:img.shape[0], 0:img.shape[1]][1]
 
-            shadow_mask[((X_m - top_x) * (bot_y - top_y) - (bot_x - top_x) * (Y_m - top_y) >= 0)] = 1
+            shadow_mask[((x_m - top_x) * (bot_y - top_y) -
+                         (bot_x - top_x) * (y_m - top_y) >= 0)] = 1
 
             if np.random.randint(2) == 1:
                 random_bright = .5
@@ -167,23 +169,22 @@ class Utils(object):
         return images
 
 
-utils = Utils()
-# Main Functions #
+UTILS = Utils()
 
 
-def prep_dataset(drive_info, path, use_left_right=True):
+def prep_dataset(drive_info, use_left_right=True):
     """Prepare dataset for keras model."""
-    dataset, labels = utils.load_data(drive_info, path, use_left_right)
+    dataset, labels = UTILS.load_data(drive_info, use_left_right)
 
     return dataset, labels
 
 
-def prep_trainingset(drive_info, path):
+def prep_trainingset(drive_info):
     """Prepare dataset for training."""
-    dataset, labels = prep_dataset(drive_info, path)
+    dataset, labels = prep_dataset(drive_info)
     labels = labels.astype(np.float16)
 
-    logging.info('Training dataset shape {}'.format(dataset.shape))
+    logging.info('Training dataset shape %s', dataset.shape)
     dataset_size = labels.shape[0]
 
     return dataset, labels, dataset_size
@@ -197,23 +198,23 @@ def train_generator(dataset, labels, batch_size):
     while True:
         end = start + batch_size
 
-        d = np.copy(dataset)
-        l = np.copy(labels)
+        _dataset = np.copy(dataset)
+        _labels = np.copy(labels)
 
         if end <= dataset_size:
-            d, l = d[start:end], l[start:end]
+            _dataset, _labels = _dataset[start:end], _labels[start:end]
         else:
             diff = end - dataset_size
-            d = np.concatenate((d[start:], d[0:diff]), axis=0)
-            l = np.concatenate((l[start:], l[0:diff]), axis=0)
+            _dataset = np.concatenate((_dataset[start:], _dataset[0:diff]), axis=0)
+            _labels = np.concatenate((_labels[start:], _labels[0:diff]), axis=0)
             start = 0
 
-        d, l = utils.random_shifts(d, l, 22, 16)
-        d = utils.random_brightness(d)
-        d = utils.random_shadows(d)
+        _dataset, _labels = UTILS.random_shifts(_dataset, _labels, 22, 16)
+        _dataset = UTILS.random_brightness(_dataset)
+        _dataset = UTILS.random_shadows(_dataset)
 
         start += batch_size
-        yield d, l
+        yield _dataset, _labels
 
 
 def model(data):
@@ -224,61 +225,65 @@ def model(data):
 
     dshape = data.shape
 
-    model = Sequential()
-    model.add(BatchNormalization(input_shape=(dshape[1], dshape[2], dshape[3])))
-    model.add(Conv2D(24, (5, 5), padding='valid', activation='relu', strides=(2, 2)))
-    model.add(Conv2D(36, (5, 5), padding='valid', activation='relu', strides=(2, 2)))
-    model.add(Conv2D(48, (5, 5), padding='valid', activation='relu', strides=(2, 2)))
-    model.add(Conv2D(64, (3, 3), padding='valid', activation='relu'))
-    model.add(Conv2D(64, (3, 3), padding='valid', activation='relu'))
-    model.add(Flatten())
-    model.add(Dense(100, activation='relu'))
+    seq_model = Sequential()
+    seq_model.add(BatchNormalization(input_shape=(dshape[1], dshape[2], dshape[3])))
+    seq_model.add(Conv2D(24, (5, 5), padding='valid', activation='relu', strides=(2, 2)))
+    seq_model.add(Conv2D(36, (5, 5), padding='valid', activation='relu', strides=(2, 2)))
+    seq_model.add(Conv2D(48, (5, 5), padding='valid', activation='relu', strides=(2, 2)))
+    seq_model.add(Conv2D(64, (3, 3), padding='valid', activation='relu'))
+    seq_model.add(Conv2D(64, (3, 3), padding='valid', activation='relu'))
+    seq_model.add(Flatten())
+    seq_model.add(Dense(100, activation='relu'))
     if FLAGS.dropout == 1:
-        model.add(Dropout(0.4))
-    model.add(Dense(50, activation='relu'))
-    model.add(Dense(10, activation='relu'))
+        seq_model.add(Dropout(0.4))
+    seq_model.add(Dense(50, activation='relu'))
+    seq_model.add(Dense(10, activation='relu'))
     if FLAGS.dropout == 1:
-        model.add(Dropout(0.4))
-    model.add(Dense(1))
+        seq_model.add(Dropout(0.4))
+    seq_model.add(Dense(1))
 
-    model.compile(loss='mse', optimizer=adam(lr=0.0001))
+    seq_model.compile(loss='mse', optimizer=adam(lr=0.0001))
 
-    return model
+    return seq_model
 
 
 logging.info('Preparing dataset/-s, may take awhile')
 
-# Training dataset and generator #
-drive_info = driving_log.read(FLAGS.train_path, 20)
-dataset, labels, dataset_size = prep_trainingset(drive_info, FLAGS.train_path)
-tg = train_generator(dataset, labels, FLAGS.batch_size)
+# Training dataset and generator
+# 165 mm wheelbasae for wltoys a969
+# https://www.banggood.com/Wltoys-A969-Rc-Car-118-2_4Gh-4WD-Short-Course-Truck-p-916962.html
+DRIVE_INFO = driving_log.read(FLAGS.train_path, 165)
+DATASET, LABELS, _ = prep_trainingset(DRIVE_INFO)
+TRAING_GEN = train_generator(DATASET, LABELS, FLAGS.batch_size)
 
-# Valid dataset and generator if val_path (validation driving log path) is provided #
+# Valid dataset and generator if val_path (validation driving log path) is provided
 if FLAGS.val_path != '':
-    val_drive_info = driving_log.read(FLAGS.val_path, 20)
-    valid_data, valid_lables = prep_dataset(val_drive_info, FLAGS.val_path, False)
+    VAL_DRIVE_INFO = driving_log.read(FLAGS.val_path, 165)
+    VALID_DATA, VALID_LABELS = prep_dataset(VAL_DRIVE_INFO, False)
 
-samples, _ = next(tg)  # load random samples necesarry for model creation
-m = model(samples)  # creates model
+SAMPLES, _ = next(TRAING_GEN)  # load random samples necesarry for model creation
+KERAS_MODEL = model(SAMPLES)  # creates model
 
-logging.info('Batch Size : {}'.format(FLAGS.batch_size))
+logging.info('Batch Size : %d', FLAGS.batch_size)
 
 if FLAGS.keras_weights != '':
     logging.info('Pretrained weights loaded')
-    m.load_weights(FLAGS.keras_weights)
+    KERAS_MODEL.load_weights(FLAGS.keras_weights)
 
 try:
     if FLAGS.val_path != '':
-        m.fit_generator(generator=tg, steps_per_epoch=FLAGS.batch_size, epochs=FLAGS.epochs, verbose=1,
-                        validation_data=(valid_data, valid_lables), pickle_safe=True, workers=12)
+        KERAS_MODEL.fit_generator(generator=TRAING_GEN, steps_per_epoch=FLAGS.batch_size,
+                                  epochs=FLAGS.epochs, verbose=1,
+                                  validation_data=(VALID_DATA, VALID_LABELS),
+                                  pickle_safe=True, workers=12)
     else:
-        m.fit_generator(generator=tg, steps_per_epoch=FLAGS.batch_size, epochs=FLAGS.epochs, verbose=1,
-                        pickle_safe=True, workers=12)
+        KERAS_MODEL.fit_generator(generator=TRAING_GEN, steps_per_epoch=FLAGS.batch_size,
+                                  epochs=FLAGS.epochs, verbose=1, pickle_safe=True, workers=12)
 
     # If output_model dir path provided save model. #
     if FLAGS.output_model != '':
-        utils.save_keras_model(m, FLAGS.output_model)
+        UTILS.save_keras_model(KERAS_MODEL, FLAGS.output_model)
 except KeyboardInterrupt:
     # Early stopping possible by interruping script. If output_model dir path provided save model. #
     if FLAGS.output_model != '':
-        utils.save_keras_model(m, FLAGS.output_model)
+        UTILS.save_keras_model(KERAS_MODEL, FLAGS.output_model)
